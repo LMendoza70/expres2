30d77f9e740cdc08493859c38366d136fbe83a2a
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-03-02 13:46:29 -0600

    Changes to be committed: 	modified:   src/models/producto.js
     Changes not staged for commit:
    	modified:   src/routes/producto.js

--- src/models/producto.js
+++ src/models/producto.js
@@ -5,15 +5,16 @@ const productoSquema=mongoose.Schema({
         type:Number,
         required:true
     },
-    presentacion:{
-        type:String,
-        required:true
+    presentacion: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Presentacions', // nombre de la colección de presentaciones
+        required: true,
     },
-    variedad:{
-        type:String,
-        required:true
+    variedad: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'varietals', // nombre de la colección de variedades
+        required: true,
     }
-
 
 })
 
