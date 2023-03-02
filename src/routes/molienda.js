de54b36a915f3fd064199108b27d9ba9bbec65db
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-03-02 09:23:42 -0600

    Changes to be committed: 	new file:   src/models/molienda.js 	new file:   src/routes/molienda.js

---
+++ src/routes/molienda.js
@@ -0,0 +1,52 @@
+const express=require('express')
+const esquema=require('../models/molienda')
+
+const router=express.Router()
+
+//crear molienda
+router.post('/moliendas',(req,res)=>{
+    const molienda= esquema(req.body)
+    molienda.save()
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message:error}))
+})
+/*
+//obtener usuarios
+router.get('/users',(req,res)=>{
+    esquema
+    .find()
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message:error}))
+})
+
+//busca user
+router.get('/users/:id',(req,res)=>{
+    const{id}=req.params;
+    esquema
+    .findById(id)
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message:error}))
+})
+
+//actualizar usuario
+router.put('/users/:id',(req,res)=>{
+    const{id}=req.params;
+    const{nombre,appaterno,apmaterno,email,password}=req.body
+
+    esquema
+    .updateOne({_id:id},{$set:{nombre,apmaterno,apmaterno,email,password}})
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message:error}))
+})
+
+//eliminar usuario
+router.delete('/users/:id',(req,res)=>{
+    const{id}=req.params;
+    esquema
+    .deleteOne({_id:id})
+    .then((data)=>res.json(data))
+    .catch((error)=>res.json({message:error}))
+})
+*/
+
+module.exports=router
