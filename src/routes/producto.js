3582e401911288cba546dd7644762c4bf5c8cb13
LMendoza70 <luis.mendoza@uthh.edu.mx>
2023-03-03 09:37:46 -0600

    Changes to be committed: 	modified:   src/routes/producto.js

--- src/routes/producto.js
+++ src/routes/producto.js
@@ -23,8 +23,8 @@ routepr.post('/producto',(req,res)=>{
 routepr.get('/producto',(req,res)=>{
     esquema
     .find()
-    //.populate('presentacion')
-    //.populate('variedad')
+    .populate('presentacions', 'descripcion')
+    .populate('varietals','nombre descripcion variedad')
     .then((data)=>res.json(data))
     .catch((error)=>res.json({message:error}))
 })
